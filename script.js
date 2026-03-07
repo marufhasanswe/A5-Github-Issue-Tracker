20c6e547efdd3a6a30d0838c6e3dc0ccb469a3d7
marufhasanswe <marufhasanswe@gmail.com>
2026-03-07 22:52:39 +0600

    nav bar added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hellow World!");
+const username = document.getElementById("username-input");
+const password = document.getElementById("password-input");
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", (event) => {
+  if (username.value == "admin" && password.value == "admin123") {
+    window.location.href = "main.html";
+  } else {
+    window.alert("Password didn't matched");
+  }
+});
