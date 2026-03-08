64e5c478867a0408efa9d6ba05ce2bfb2db56b0b
marufhasanswe <marufhasanswe@gmail.com>
2026-03-08 20:03:53 +0600

    applying dynammic style on card

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const username = document.getElementById("username-input");
 const password = document.getElementById("password-input");
-let issueCounterText = document.getElementById("issues-counter");
+const issueCounterText = document.getElementById("issues-counter");
 let issuesCounter = 0;
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("closed-btn");
 
 function submitLoginForm() {
   if (username.value == "admin" && password.value == "admin123") {
     window.location.href = "main.html";
   } else {
     window.alert("Password didn't matched");
+  }
+}
+
+function tabToggle(status) {
+  if (status == "all-btn") {
+    openBtn.classList.remove("btn-primary");
+    closeBtn.classList.remove("btn-primary");
+    allBtn.classList.add("btn-primary");
+    getIssues("all");
+  }
+  if (status == "open-btn") {
+    allBtn.classList.remove("btn-primary");
+    closeBtn.classList.remove("btn-primary");
+    openBtn.classList.add("btn-primary");
+    getIssues("open");
+  }
+  if (status == "closed-btn") {
+    allBtn.classList.remove("btn-primary");
+    openBtn.classList.remove("btn-primary");
+    closeBtn.classList.add("btn-primary");
+    getIssues("closed");
   }
 }
 
@@ -35,20 +59,19 @@ function getLabel(labels) {
 function displayData(issues) {
   issuesCounter = issues.length;
   issueCounterText.textContent = issuesCounter;
-  console.log(issuesCounter);
   const issuesCardContainer = document.getElementById("issues-card-container");
   issuesCardContainer.innerHTML = "";
   issues.forEach((e) => {
     const card = document.createElement("div");
     card.innerHTML = `
-        <div class="card shadow-md bg-white h-full">
-          <div class="border-b-2 border-gray-300 p-4">
+        <div class="card ${e.status == "open" ? "border-t-3 border-green-500" : "border-t-3 border-purple-500"} shadow-md bg-white h-full">
+          <div class="border-b-2 border-gray-200 p-4">
             <div class="flex justify-between items-center mb-3">
               <div class="status-img">
-                <img class="w-6 h-6" src="assets/Open-Status.png" alt="" />
+                <img class="w-6 h-6" src="assets/${e.status == "open" ? "Open" : "Closed"}-Status.png" alt="" />
               </div>
               <p
-                class="w-20 py-1 text-center rounded-full text-xs bg-[#FEECEC] text-[#EF4444]"
+                class="w-20 py-1 text-center rounded-full text-xs ${e.priority == "high" ? "high" : e.priority == "medium" ? "medium" : "low"}"
               >
                 ${e.priority}
               </p>
@@ -73,12 +96,23 @@ function displayData(issues) {
   });
 }
 
-async function getAllIssues() {
+async function getIssues(status) {
   const res = await fetch(
     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
   const data = await res.json();
-  displayData(data.data);
+
+  if (status == "all") {
+    displayData(data.data);
+  }
+  if (status == "open") {
+    const opens = data.data.filter((e) => e.status == "open");
+    displayData(opens);
+  }
+  if (status == "closed") {
+    const closes = data.data.filter((e) => e.status == "closed");
+    displayData(closes);
+  }
 }
 
-getAllIssues();
+getIssues("all");
